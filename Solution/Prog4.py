f28b99d4c8ac6a330cb7ec95c194be3649ac1613
21bca67 <96829494+21bca67@users.noreply.github.com>
2022-03-09 21:02:55 +0530

    Update Prog4.py

--- Solution/Prog4.py
+++ Solution/Prog4.py
@@ -1,3 +1,5 @@
+#WAS to enter 3 number and print which number maximum number.
+
 a = int(input("Enter First value: "))
 b = int (input("Enter Second value: "))
 c = int(input("Enter third value: "))
